166b8101e747b2bdf1102fc306a5e4b8e62064ba
Bemuseed <calebmullan@hotmail.com>
2023-04-19 14:25:44 +0100

    Patched in typewriter effect and original scene logic

--- scene.js
+++ scene.js
@@ -16,37 +16,31 @@ function typeWriter(txt, element_id, speed) {
 }
 
 class ChoiceOption {
-	constructor(button_text, next_scene, log_text="") {
+	constructor(button_text, next_scene){
 		this.button_text = button_text;
 		this.next_scene = next_scene;
-		this.log_text = log_text;
-		if (this.log_text == "") {
-			this.log_text = this.button_text;
-		}
 	}
 }
 
 class Scene {
-	constructor(prompt_text, choice_options, next_scenes) {
+	constructor(prompt_text, choice_options) {
 		this.prompt_text = prompt_text;
 		this.choice_options = choice_options;
-		this.next_scenes = next_scenes;
 	}
 
 	display() {	
-		typeWriter(this.prompt_text, "prompt-text", 20);
-		for (let i=0; i < this.choice_options.length; i++) {
-			document.getElementById("input-text").innerHTML += "<button>" + this.choice_options[i].button_text + "</button>";
+		typeWriter(this.prompt_text, "sceneText", 20);
+		console.log(this.choice_options.length);
+		for(let i=0; i < this.choice_options.length; i++) {
+			console.log(":)");
+			document.getElementById("promptBox").innerHTML += "<button>" + this.choice_options[i].button_text + "</button>";	
 		}
-
 	}
-
 }
 
 function run(){
-	var choice1 = new ChoiceOption("Hide", 0);
-	var choice2 = new ChoiceOption("Run", 0);
-	var choice3 = new ChoiceOption("Pick up", 0);
-	s = new Scene(text, [choice1, choice2, choice3], 3);
+	choice1 = new ChoiceOption("I'm gonna die", 0)
+	choice2 = new ChoiceOption("I got this", 0)
+	s = new Scene(text,[choice1, choice2]);
 	s.display();
 }
