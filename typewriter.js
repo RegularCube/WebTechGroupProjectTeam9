59fc7fe7c3bb078333b32a2e961b7813bf4764b3
Bemuseed <calebmullan@hotmail.com>
2023-04-20 11:42:17 +0100

    Fix for multiple simultaneous typewriter effects

--- typewriter.js
+++ typewriter.js
@@ -2,9 +2,14 @@ function typeWriter(txt, element_id, speed=10) {
 	var i = 0;
 
 	(function(){
-		document.getElementById(element_id).innerHTML += txt.charAt(i++);
-		if(txt.length > i){
-			setTimeout(arguments.callee, speed);
+		try { // Hack-y fix for multiple typewriter's going at once
+			document.getElementById(element_id).innerHTML += txt.charAt(i++);
+			if(txt.length > i){
+				setTimeout(arguments.callee, speed);
+			}
 		}
-    	})();
+		catch {
+			console.log("Scene fast-forwarded.")
+		}
+			})();	
 }
